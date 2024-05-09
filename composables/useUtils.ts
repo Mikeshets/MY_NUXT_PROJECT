4f8a185cde37372928a89674b75424a47dcfb561
MikeshETS <mikesh.easternts@gmail.com>
2024-05-09 11:23:52 +0530

    Composables Understandings

---
+++ composables/useUtils.ts
@@ -0,0 +1,8 @@
+export const useUtils = () => {
+  const sayHello = () => console.log("Hello");
+
+  return { sayHello };
+};
+
+
+// VueUse composable library provides lots of prebuild composables ready to use
