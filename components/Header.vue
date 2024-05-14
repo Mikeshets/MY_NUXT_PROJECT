119ae67d45d5ce4eae7608dd7d26481388ac570b
MikeshETS <mikesh.easternts@gmail.com>
2024-05-14 12:47:47 +0530

    Remove unused components and pages
    Remove unused components and pages from the project. This includes removing
    files related to Events, Profile, Pinia, Repo API, Login, and Vuetify.

--- components/Header.vue
+++ components/Header.vue
@@ -2,13 +2,9 @@
   <div>
     <header class="flex justify-start items-center gap-3 px-4 py-2 bg-gray-500">
       <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/Events">Events</nuxt-link>
-      <nuxt-link to="/Profile">Profile</nuxt-link>
       <nuxt-link to="/state">State</nuxt-link>
       <nuxt-link to="/repotest">Repo-Test</nuxt-link>
       <nuxt-link to="/users">Users-Test</nuxt-link>
-      <nuxt-link to="/pinia">Pinia</nuxt-link>
-      <nuxt-link to="/repoapi">Repo</nuxt-link>
     </header>
   </div>
 </template>
