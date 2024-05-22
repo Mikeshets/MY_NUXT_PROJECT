d491aa37ad6d510c0737fb4dcad917f9c2b42e9b
MikeshETS <mikesh.easternts@gmail.com>
2024-05-22 10:55:58 +0530

    Refactor router parameter retrieval in products API handler
    This commit replaces the usage of optional chaining to retrieve the 'id'
    parameter from the router context with a call to the 'getRouterParam'
    function for consistency and better error handling.

--- server/api/products/[id].ts
+++ server/api/products/[id].ts
@@ -2,7 +2,7 @@ import { H3Event } from "h3";
 
 export default defineEventHandler(async (event: H3Event) => {
   const baseURL = process.env.API_BASE_URL;
-  const id = event.context.params?.id;
+  const id = getRouterParam(event, "id");
   const apiURL = `${baseURL}/products/${id}`;
   const method = event.node.req.method;
 
